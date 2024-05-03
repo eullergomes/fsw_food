a7012328c8f9c4a300e0eebfc8c6fb7a333dc21d
eullergomes <eullertexeira@gmail.com>
2024-05-03 16:12:54 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -13,6 +14,18 @@ export default function Home() {
 
       <div className="px-5 py-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5-pt6">
+        <Image
+          src="/banner-promo-pizza.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="object-cotain h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </div>
   );
