47592f222a3d7bf6a8b0aa20a093fcf877740ac2
eullergomes <eullertexeira@gmail.com>
2024-05-04 17:29:08 -0300

    feat: add restaurant list component

---
+++ app/_components/restaurant-item.tsx
@@ -0,0 +1,62 @@
+import { Restaurant } from "@prisma/client";
+import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
+
+interface RestaurantItemProps {
+  restaurant: Restaurant;
+}
+
+const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
+  return (
+    <div className="min-w-[266px] space-y-2">
+      {/* IMAGE */}
+      <div className="relative h-[136px] w-full">
+        <Image
+          src={restaurant.imageUrl}
+          alt={restaurant.name}
+          fill
+          className="rounded-lg object-cover shadow-md"
+        />
+
+        <Badge className="absolute left-2 top-2 flex items-center gap-1 bg-white px-1 font-semibold transition-opacity duration-300 ease-in-out hover:bg-white hover:opacity-90">
+          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+          <span className="text-xs text-black">5.0</span>
+        </Badge>
+
+        <Button
+          size="icon"
+          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+        >
+          <HeartIcon size={16} className="fill-white" />
+        </Button>
+      </div>
+      {/* RESTAURANT */}
+      <div>
+        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+        <div className="flex gap-3">
+          <div className="flex items-center gap-1">
+            <BikeIcon className="text-primary" size={12} />
+
+            <span className="text-xs text-muted-foreground">
+              {Number(restaurant.deliveryFee) === 0
+                ? "Entrega grátis"
+                : formatCurrency(Number(restaurant.deliveryFee))}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <TimerIcon className="text-primary" size={12} />
+            <span className="text-xs text-muted-foreground">
+              {restaurant.deliveryTime} min
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantItem;
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   //products with a discount percentage greater than zero
@@ -64,6 +65,20 @@ const Home = async () => {
           src="/banner-promo-burger.png"
           alt="A partir de 17,90 em lanches"
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            <span>Ver todos</span>
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </div>
   );
