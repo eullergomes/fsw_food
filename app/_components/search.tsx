7f04e17c415947cf3b9e20e6d993a08dc877bf69
eullergomes <eullertexeira@gmail.com>
2024-05-09 11:06:39 -0300

    feat: ensure restaurants can be searched for

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,15 +1,45 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleChance = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    router.push(`/restaurants?search=${search}`);
+  };
+
+  // const handleKeyPress:KeyboardEventHandler<HTMLInputElement> = (e) => {
+  //   if (e.key === 'Enter') {
+  //     handleSearchSubmit(e);
+  //   }
+  // }
+
   return (
-    <div className="flex gap-2">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
-      <Button size="icon">
+    <form className="flex gap-2" onSubmit={handleSearchSubmit}>
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none"
+        onChange={handleChance}
+        // onKeyDown={handleKeyPress}
+        value={search}
+      />
+      <Button type="submit" size="icon">
         <SearchIcon size={20} />
       </Button>
-    </div>
+    </form>
   );
 };
 
