8f98e7741f76caf3d98b5e72cd166c776eda5000
eullergomes <eullertexeira@gmail.com>
2024-05-28 22:21:00 -0300

    refactor: transform authoptins in object

---
+++ app/_lib/auth.ts
@@ -0,0 +1,23 @@
+import { AuthOptions } from "next-auth";
+import { db } from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+//use for get and post request
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  //add id in user in the session
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id };
+      return session;
+    },
+  },
+};
