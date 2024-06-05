103a5e89d1927ece6aa72e37feee65e446c11acb
eullergomes <eullertexeira@gmail.com>
2024-06-05 12:29:23 -0300

    feat: add ensure restaurant favorited

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -1,4 +1,6 @@
-import { Restaurant } from "@prisma/client";
+"use client";
+
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { Badge } from "./ui/badge";
@@ -6,39 +8,72 @@ import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { cn } from "../_lib/utils";
+import { toggleFavoriteRestaurant } from "../_actions/restaurant";
+import { toast } from "sonner";
 
 interface RestaurantItemProps {
+  userId?: string;
   restaurant: Restaurant;
   className?: string;
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
 }
 
-const RestaurantItem = ({ restaurant, className }: RestaurantItemProps) => {
+const RestaurantItem = ({
+  userId,
+  restaurant,
+  className,
+  userFavoriteRestaurants,
+}: RestaurantItemProps) => {
+  const isFavorite = userFavoriteRestaurants.some(
+    (favorite) => favorite.restaurantId === restaurant.id,
+  );
+
+  const handleFavoriteClick = async () => {
+    if (!userId) return;
+
+    try {
+      await toggleFavoriteRestaurant(userId, restaurant.id);
+      toast.success(
+        isFavorite
+          ? "Restaurante removido dos favoritos!"
+          : "Restaurante favoritado!",
+      );
+    } catch (error) {
+      toast.error("Erro ao favoritar restaurante!");
+    }
+  };
+
   return (
-    <Link
-      href={`/restaurants/${restaurant.id}`}
-      className={cn("min-w-[266px] max-w-[266px]", className)}
-    >
+    <div className={cn("min-w-[266px] max-w-[266px]", className)}>
       <div className="w-full space-y-2">
         {/* IMAGE */}
         <div className="relative h-[136px] w-full">
-          <Image
-            src={restaurant.imageUrl}
-            alt={restaurant.name}
-            fill
-            className="rounded-lg object-cover shadow-md"
-          />
+          <Link href={`/restaurants/${restaurant.id}`}>
+            <Image
+              src={restaurant.imageUrl}
+              alt={restaurant.name}
+              fill
+              className="rounded-lg object-cover shadow-md"
+            />
+          </Link>
 
           <Badge className="absolute left-2 top-2 flex items-center gap-1 bg-white px-1 font-semibold transition-opacity duration-300 ease-in-out hover:bg-white hover:opacity-90">
             <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
             <span className="text-xs text-black">5.0</span>
           </Badge>
 
-          <Button
-            size="icon"
-            className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
-          >
-            <HeartIcon size={16} className="fill-white" />
-          </Button>
+          {userId && (
+            <Button
+              size="icon"
+              className={`absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700 ${isFavorite && "bg-primary hover:bg-gray-700"}`}
+            >
+              <HeartIcon
+                size={16}
+                className="fill-white"
+                onClick={handleFavoriteClick}
+              />
+            </Button>
+          )}
         </div>
         {/* RESTAURANT */}
         <div>
@@ -63,7 +98,7 @@ const RestaurantItem = ({ restaurant, className }: RestaurantItemProps) => {
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
