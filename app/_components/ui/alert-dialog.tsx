506b3324038360ea467b74b3570427687ca9246b
eullergomes <eullertexeira@gmail.com>
2024-06-19 08:55:21 -0300

    feat: ensure close alertdialog

--- app/_components/ui/alert-dialog.tsx
+++ app/_components/ui/alert-dialog.tsx
@@ -64,10 +64,7 @@ const AlertDialogFooter = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-center sm:space-x-2",
-      className,
-    )}
+    className={cn("flex items-center justify-center space-x-2", className)}
     {...props}
   />
 );
